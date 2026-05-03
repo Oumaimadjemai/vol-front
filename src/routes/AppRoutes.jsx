941838c4ab381e36d943a338b6de62f01216e399
narin2120 <n.taibi@esi-sba.dz>
2026-05-03 13:03:37 +0100

    push paiement

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,14 +1,3 @@
-import { Routes, Route } from "react-router-dom";
-
-import AuthCallback from "../Components/authentication/AuthCallback";
-import FlightSearch from "../Components/Vol/SearchFlight/FlightSearchPage";
-import ResultatSearch from "../Components/Vol/ResultatSearchePage/ResultatSearch";
-export default function AppRoutes() {
-  return (
-    <Routes>
-     <Route path="/results" element={<ResultatSearch />} />
-     <Route path="/vol" element={<FlightSearch/>} />
-     <Route path="/auth/callback" element={<AuthCallback />} />
 import Login from "../Components/authentication/Login";
 import Signup from "../Components/authentication/Signup";
 import ForgotPassword from "../Components/authentication/ResetPassword";
